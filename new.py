91720ca1b4b3fba38e95b6080d938458720c8164
VolodymyrTES <135321689+VolodymyrTES@users.noreply.github.com>
2023-07-25 09:50:58 +0300

    Update new.py

--- new.py
+++ new.py
@@ -9,7 +9,6 @@ documents =  ['doc', 'docs', 'txt', 'pdf', 'xlsx', 'pptx', 'ppt']
 music =  ['mp3', 'ogg', 'wav', 'amr','wma']
 archive = ['zip', 'gz', 'tar', 'rar']
 
-
 CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
 TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u","f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
 
@@ -34,8 +33,8 @@ def sorting_files(path):
             filename, extension = os.path.splitext(i)
 
             if extension[1:].lower() in pictures:
-                new_name = normalize(filename)
-                              
+                
+                new_name = normalize(filename)                              
                 if os.path.isdir(path):
                     if os.path.exists(path+'/'+"PICTURES"):
                         shutil.move(filePath[0]+'/'+ i, path+'/'+"PICTURES"+'/'+ f"{new_name}{extension}")
@@ -45,8 +44,8 @@ def sorting_files(path):
 
 
             elif extension[1:].lower() in videos:
-                new_name = normalize(filename)
-               
+                
+                new_name = normalize(filename)               
                 if os.path.isdir(path):
                     if os.path.exists(path+'/'+"VIDEO"):
                         shutil.move(filePath[0]+'/'+ i, path+'/'+"VIDEO"+'/'+ f"{new_name}{extension}")
@@ -55,8 +54,8 @@ def sorting_files(path):
                         shutil.move(filePath[0]+'/'+ i, path+'/'+"VIDEO"+'/'+ f"{new_name}{extension}")
                 
             elif extension[1:].lower() in documents:
-                new_name = normalize(filename)
-               
+                
+                new_name = normalize(filename)               
                 if os.path.isdir(path):
                     if os.path.exists(path+'/'+"DOCUMENTS"):
                         shutil.move(filePath[0]+'/'+ i, path+'/'+"DOCUMENTS"+'/'+ f"{new_name}{extension}")
@@ -65,8 +64,8 @@ def sorting_files(path):
                         shutil.move(filePath[0]+'/'+ i, path+'/'+"DOCUMENTS"+'/'+ f"{new_name}{extension}")
 
             elif extension[1:].lower() in music:
-                new_name = normalize(filename)
-               
+                
+                new_name = normalize(filename)            
                 if os.path.isdir(path):
                     if os.path.exists(path+'/'+"MUSIC"):
                         shutil.move(filePath[0]+'/'+ i, path+'/'+"MUSIC"+'/'+ f"{new_name}{extension}")
@@ -76,8 +75,8 @@ def sorting_files(path):
             
                         
             elif extension[1:].lower() in archive:
-                new_name = normalize(filename)
-               
+                
+                new_name = normalize(filename)               
                 if os.path.isdir(path):
                     if os.path.exists(path+'/'+"ARCHIVE"):
                         os.makedirs(path + '/'+"ARCHIVE"+'/'+ new_name)
@@ -89,8 +88,8 @@ def sorting_files(path):
                         shutil.unpack_archive(filePath[0]+'/'+ i, path+'/'+"ARCHIVE"+'/'+ new_name)
                         shutil.move(filePath[0]+'/'+ i, path+'/'+"ARCHIVE"+'/'+ f"{new_name}{extension}")
             else:
+                
                 new_name = normalize(filename)
-               
                 if os.path.isdir(path):
                     if os.path.exists(path+'/'+"UNKNOW"):
                         shutil.move(filePath[0]+'/'+ i, path+'/'+"UNKNOW"+'/'+ f"{new_name}{extension}")
@@ -100,7 +99,4 @@ def sorting_files(path):
         if len(os.listdir(filePath[0])) == 0: # Check if the folder is empty
             shutil.rmtree(filePath[0])# If so, delete it  
         
-
-
-if __name__ == "__main__":
-    sorting_files(path = input('Enter a path of the folder you want to sort out: ')) # будемо водити шлях папки яку хочемо сортувати
+if __name__ == "__main__": sorting_files(path = input('Enter a path of the folder you want to sort out: ')) 
